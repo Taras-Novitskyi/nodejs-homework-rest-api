5712b88983f8e32bc2f0af7cc354d162ce7838b6
Taras-Novitskyi <novitskyitaras@gmail.com>
2023-03-12 15:22:57 +0200

    fixed

--- service/schemas/contacts.js
+++ service/schemas/contacts.js
@@ -31,8 +31,7 @@ const contact = new Schema(
 const contactSchema = Joi.object({
   name: Joi.string().min(3).max(30).required(),
   email: Joi.string()
-    .email({ minDomainSegments: 2, tlds: { allow: ["com", "net"] } })
-    .required(),
+    .email({ minDomainSegments: 2, tlds: { allow: ["com", "net"] } }),
   phone: Joi.string().required(),
   favorite: Joi.boolean(),
 });
